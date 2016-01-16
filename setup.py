2c7917057faaabfb8a37d05eb9c20dc38df8d30c
Mario Barrenechea <mbarrenecheajr@gmail.com>
2016-01-15 17:10:51 -0700

    -forgot the version numbering and release URL. sheesh.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'crowdrouter',
   packages = ['crowdrouter', 'crowdrouter/task', 'crowdrouter/workflow'], # this must be the same as the name above
-  version = '0.4',
+  version = '0.5',
   description = 'A framework for architecting tasks to the crowd.',
   author = 'Mario Barrenechea',
   author_email = 'mbarrenecheajr@gmail.com',
   url = 'https://github.com/Project-EPIC/crowdrouter', # use the URL to the github repo
-  download_url = 'https://github.com/Project-EPIC/crowdrouter/archive/1.0.tar.gz',
+  download_url = 'https://github.com/Project-EPIC/crowdrouter/archive/0.5.tar.gz',
   keywords = ['crowdsourcing', 'tasks', 'workflows'], # arbitrary keywords
   classifiers = [],
 )
