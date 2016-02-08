be533f335633a8ddaa0eb2a984d4b879a8cb9fc6
Mario Barrenechea <mbarrenecheajr@gmail.com>
2016-02-08 12:15:24 -0700

    [Hot fix]: Removed Django's request.REQUEST attribute as it is deprecated and causing problems when updating the data attribute in RequestStrategy. -Version is 1.5.1

--- crowdrouter/context/django_request_strategy.py
+++ crowdrouter/context/django_request_strategy.py
@@ -7,8 +7,8 @@ class DjangoRequestStrategy(abstract_request_strategy.AbstractRequestStrategy):
             self.request = request
             self.session = request.session
             self.path = request.path
-            self.data = request.REQUEST or request.GET or {}
             self.method = request.method
+            self.data = request.GET or {}
             self.form = request.POST or request.FILES or {}
         except:
             raise InvalidRequestError(value="DjangoRequestStrategy cannot properly bind all needed variables for request %s." % request)
